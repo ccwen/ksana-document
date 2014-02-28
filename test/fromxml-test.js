0a15e5159911e7e6e659efff312aec96c1a44ea4
yap cheah shen <yapcheahshen@gmail.com>
2014-02-28 21:24:29 +0530

    prepare for reunit

--- test/fromxml-test.js
+++ test/fromxml-test.js
@@ -6,7 +6,7 @@ QUnit.test('import xml',function(){
 	var buf=fs.readFileSync('./test1.xml','utf8').replace(/\r\n/g,'\n').replace(/\r/g,'\n');
 	var doc=F.importXML(buf,{"template":"accelon"});
 	equal(doc.getPageCount(),4);
-	equal(doc.xmltags.length,7);
+	equal(doc.xmltags.length,11);
 	console.log(doc.getPage(2).getInscription())
 	equal(doc.xmltags[0].name,'xml')
 	equal(doc.xmltags[1].attr.t,'xyz')
@@ -14,3 +14,8 @@ QUnit.test('import xml',function(){
 	console.log(F.formatJSON(doc.xmltags,doc.getMeta()));
 });
 
+QUnit.test('reunit',function(){
+	var buf=fs.readFileSync('./test1.xml','utf8').replace(/\r\n/g,'\n').replace(/\r/g,'\n');
+	var doc=F.importXML(buf,{"template":"accelon"});
+	equal(true,true)
+})
