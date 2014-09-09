2c6dc5f9faad94c36e69ef5b0ce7e21d937b4bc5
yapcheahshen <yapcheahshen@gmail.com>
2014-09-09 19:05:06 +0800

    save markup in localStorage

--- persistentmarkup.js
+++ persistentmarkup.js
@@ -1,10 +1,35 @@
 //saveMarkup({dbid:dbid,markups:markups,filename:filename,i:this.state.pageid } ,function(data){
+//TODO , change to pouchdb
+var combineMarkups=function(markups,fn,pageid) {
+	var existing=JSON.parse(localStorage.getItem("M!"+pageid+"!"+fn));
+	if (!markups || !markups.length) return existing;
 
-var saveMarkup=function(payload,cb){
-	for (var i=0;i<payload.markups.length;i++) {
-		payload.markups[i].i=payload.i;
+	var author=markups[0].payload.author, others=[];
+	var mfn=fn+'m';
+
+	if (existing) {
+		others=existing.filter(function(m){return m.i!=pageid || m.payload.author != author});		
 	}
-	localStorage.setItem("M!"+payload.i+"!"+payload.filename,JSON.stringify(payload.markups));
+	
+	for (var i=0;i<markups.length;i++) {
+		markups[i].i=pageid;
+	}
+	others=others.concat(markups);
+	var sortfunc=function(a,b) {
+		//each page less than 64K
+		return (a.i*65536 +a.start) - (b.i*65536 +b.start);
+	}
+	others.sort(sortfunc);
+	return others;
+}
+
+var saveMarkup=function(opts,cb){
+	var markups=combineMarkups(opts.markups,opts.filename,opts.i);
+
+	for (var i=0;i<markups.length;i++) {
+		markups[i].i=opts.i;
+	}
+	localStorage.setItem("M!"+opts.i+"!"+opts.filename,JSON.stringify(markups));
 	setTimeout(cb,1);
 }
 var __loadMarkups=function(key,pagecount,cb) {
